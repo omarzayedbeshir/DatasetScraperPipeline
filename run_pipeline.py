94c97b13021d14b7cac137ce080cb1d3764b5588
Omar Zayed Beshir <omarzayed7777@gmail.com>
2026-03-26 09:17:44 +0200

    fixed minor spelling mistake

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-scripts = ["generate_urls.py", "dataset_url_scraper.py", "scrapery.py"]
+scripts = ["generate_urls.py", "dataset_url_scraper.py", "scraper.py"]
 
 
 print("Running run_pipeline.py...")
